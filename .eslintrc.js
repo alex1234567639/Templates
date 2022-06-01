f2f4345d4fc572dcb6d7d5ffbc5d17eec0e9c2cc
alexWangfg <alexwang@futuregame.com.tw>
2022-06-01 17:22:13 +0800

    [feature]新增eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
