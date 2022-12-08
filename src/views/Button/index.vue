00b103d159748f4a9662ffa2119e4205b09f0e5b
alexWangfg <alexwang@futuregame.com.tw>
2022-12-08 11:48:58 +0800

    [feature]新增按鈕樣式

---
+++ src/views/Button/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Header />
+    <div class="button-box">
+      <button class="button"> Button </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header/index'
+export default {
+  components: {
+    Header
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.button-box {
+  padding: 90px 20px;
+  & .button {
+    @apply cursor-pointer;
+    border: 1px solid black;
+    outline: none;
+    width: 180px;
+    height: 60px;
+    line-height: 60px;
+    border-radius: 10px;
+    font-size: 20px;
+    letter-spacing: 3px;
+  }
+}
+</style>
